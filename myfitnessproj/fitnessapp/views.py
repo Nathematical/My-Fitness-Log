faa982d2972215a6cd0e4392632d75078c4bba9c
Adam Meier <meieradamn@gmail.com>
2020-06-29 16:19:26 -0700

    fitnessproj progress

--- myfitnessproj/fitnessapp/views.py
+++ myfitnessproj/fitnessapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    context = {
+        'message': 'Hello World!'
+    }
+    return render(request, 'fitnessapp/index.html', context)
