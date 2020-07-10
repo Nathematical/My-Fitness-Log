6e2f6fd4702d9ab1ca6e676cd5def875d39ae2cb
Adam Meier <meieradamn@gmail.com>
2020-07-10 11:44:02 -0700

    updates to templates

--- fitness_log/log_users/urls.py
+++ fitness_log/log_users/urls.py
@@ -5,4 +5,6 @@ app_name = 'log_users'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
--- fitness_log/log_users/views.py
+++ fitness_log/log_users/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def register(request):
-    return HttpResponse('User Registration')
+    return render(request, 'log_users/register.html')
 
 
 def login(request):
     return HttpResponse('User Login')
+
+
+def logout(request):
+    return HttpResponse('User logout')
+
+
+def profile(request):
+    return HttpResponse('User Profile')
