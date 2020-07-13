e800f7d656cba88969ec90bd57109cd1e020afcd
Adam Meier <meieradamn@gmail.com>
2020-07-13 14:01:50 -0700

    updates to log_users and log_app models

--- fitness_log/log_users/views.py
+++ fitness_log/log_users/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 def register(request):
-    return render(request, 'log_users/register.html')
+    # print(request.method)
+    if request.method == 'POST':
+        # print(request.POST)
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['pasword']
+        retype_pw = request.POST['retype_pw']
+
+        if password != retype_pw:
+            return render(request, 'log_user/register.html', )
+
+        user = User.objects_user(username, email, password)
+    return render(request, 'log_users/register.html', {'warning': 'Passwords don\'t match.'})
 
 
 def login(request):
