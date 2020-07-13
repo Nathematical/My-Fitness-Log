9d5dfe7a844e88450c16c55be639f098918607b6
Adam Meier <meieradamn@gmail.com>
2020-07-13 14:48:04 -0700

    fixed views.py errors

--- fitness_log/log_users/views.py
+++ fitness_log/log_users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import User
+import django.contrib.auth
 
 def register(request):
     # print(request.method)
@@ -8,14 +9,20 @@ def register(request):
         # print(request.POST)
         username = request.POST['username']
         email = request.POST['email']
-        password = request.POST['pasword']
+        password = request.POST['password']
         retype_pw = request.POST['retype_pw']
 
-        if password != retype_pw:
-            return render(request, 'log_user/register.html', )
+        if User.objects.filter(username=username).exists():
+            return render(request, 'log_users/register.html', {'warning': 'That username already exists.'})
 
-        user = User.objects_user(username, email, password)
-    return render(request, 'log_users/register.html', {'warning': 'Passwords don\'t match.'})
+        if password != retype_pw:
+            return render(request, 'log_users/register.html', {'warning': 'Passwords don\'t match.'})
+
+        user = User.objects.create_user(username, email, password)
+        django.contrib.auth.login(request, user)
+        return HttpResponseRedirect(reverse('log_users:profile'))
+    
+    return render(request, 'log_users/register.html')
 
 
 def login(request):
