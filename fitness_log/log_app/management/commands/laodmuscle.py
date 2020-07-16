f26a8bbdf4624b091b475ad1d545ba318078bf3e
Adam Meier <meieradamn@gmail.com>
2020-07-15 17:11:56 -0700

    updates to templates

--- fitness_log/log_app/management/commands/laodmuscle.py
+++ fitness_log/log_app/management/commands/laodmuscle.py
@@ -1,8 +1,8 @@
-from django.core.management.base import BaseCommand
-from ...models import MuscleGroup, Exercise
-import requests
+# from django.core.management.base import BaseCommand
+# from ...models import MuscleGroup, Exercise
+# import requests
 
-class Command(BaseCommand):
+# class Command(BaseCommand):
 
-    def handle(self, *args, **options):
-        pass
+#     def handle(self, *args, **options):
+#         pass
--- fitness_log/log_app/urls.py
+++ fitness_log/log_app/urls.py
@@ -5,5 +5,5 @@ app_name = 'log_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('main/', views.main, name='main'),
-    path('new_workout/', views.new_workout, name='newworkout')
+    path('new_workout/', views.new_workout, name='new_workout')
 ]
