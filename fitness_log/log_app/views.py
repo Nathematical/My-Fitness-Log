ac1598fea464aa9dc1eb33046881e7bd2b885cf1
Adam Meier <meieradamn@gmail.com>
2020-07-16 09:54:13 -0700

    added workout_history.html

--- fitness_log/log_app/views.py
+++ fitness_log/log_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from .models import Exercise
 
 def index(request):
     return HttpResponse('This is the index page.')
@@ -9,3 +10,6 @@ def main(request):
 
 def new_workout(request):
     return render(request, 'log_app/workout.html')
+
+def workout_history(request):
+    return render(request, 'log_app/workout_history.html')
